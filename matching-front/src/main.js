11f6448730898463ac59bd886c640aae439a8e8d
“yoshi_0202“ <kobayashi020202@gmail.com>
2019-10-31 07:51:21 +0900

    add axios & change styles

--- matching-front/src/main.js
+++ matching-front/src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    axios,
+    render: h => h(App)
 }).$mount("#app");
