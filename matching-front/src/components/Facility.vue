e3d7be73254ffdcd6c9d3a479d37e0d476827684
“yoshi_0202“ <kobayashi020202@gmail.com>
2019-11-02 11:29:47 +0900

    change top page

---
+++ matching-front/src/components/Facility.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto mb-3" max-width="700" v-for="i in 10" :key="i">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title>サンプル施設</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pb-0">施設の説明</v-card-subtitle>
+
+      <v-layout text-left wrap>
+        <v-card-text class="text--primary">
+          <div>説説明説明説明説明説明説明説明説明説明説明説明説明説明明</div>
+
+          <div>説説明説明説明説明説明説明説明説明説明説明説明説明説明明</div>
+        </v-card-text>
+      </v-layout>
+      <v-card-actions>
+        <v-layout text-right wrap>
+          <v-btn color="orange" text>この施設に問い合わせする</v-btn>
+        </v-layout>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
---
+++ matching-front/src/components/SearchForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-flex xs12>
+        <v-card class="text-left">
+          <p>施設情報を検索する</p>
+        </v-card>
+        <v-text-field
+          v-model="firstname"
+          :rules="nameRules"
+          :counter="10"
+          label="First name"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-text-field
+          v-model="lastname"
+          :rules="nameRules"
+          :counter="10"
+          label="Last name"
+          required
+        ></v-text-field>
+      </v-flex>
+      <v-flex xs12>
+        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+      </v-flex>
+    </v-container>
+  </v-form>
+</template>
