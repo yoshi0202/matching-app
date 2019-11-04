ea489c31429b7296c422fca7bada6c28e53ade88
“yoshi_0202“ <kobayashi020202@gmail.com>
2019-11-04 11:25:55 +0900

    add facility create

---
+++ db/migrate/20191104021642_create_facilities.rb
@@ -0,0 +1,10 @@
+class CreateFacilities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :facilities do |t|
+      t.string :name
+      t.text :detail
+
+      t.timestamps
+    end
+  end
+end
