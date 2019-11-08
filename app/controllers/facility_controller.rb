f108d820081527b10ffa466696401be1d5c308c6
“yoshi_0202“ <kobayashi020202@gmail.com>
2019-11-09 00:33:05 +0900

    fin facility crud

--- app/controllers/facility_controller.rb
+++ app/controllers/facility_controller.rb
@@ -36,4 +36,26 @@ class FacilityController < ApplicationController
       'facilities' => facilities,
     }
   end
+  def update
+    images = params[:facilityImages]
+    public_path = 'public/facility_images/'
+    facility = Facility.find(params[:facilityId])
+    facility.update!(
+      "name" => params[:facilityName],
+      "detail" => params[:facilityDetail],
+    )
+    if images.present?
+      facility.facility_images.destroy_all
+      images.each do |image|
+        facility.facility_images.create!(
+          "name" => image.original_filename
+        )
+        output_path = Rails.root.join(public_path, image.original_filename)
+        File.open(output_path, 'w+b') do |fp|
+          fp.write  image.read
+        end
+      end
+    end
+    render :json => {'result' => 'OK'}
+  end
 end
